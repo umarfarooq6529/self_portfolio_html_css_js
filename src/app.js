81bb32b456004ab212b0d182610c368d44c7ba02
Umar Farooq <100484772+umarfarooq6529@users.noreply.github.com>
2023-10-13 23:20:04 +0500

    complete section_1 with slide transition

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 // applying fade transition on .hidden elements
-const hiddenElements = document.querySelectorAll('.hidden');
+const fadeElements = document.querySelectorAll('.hidden');
 
-const observer = new IntersectionObserver((entries) => {
+const section_main = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry);
 
-        if (entry.isIntersecting) entry.target.classList.add('show');
-        else entry.target.classList.remove('show')
+        if (entry.isIntersecting) entry.target.classList.add('fade');
+        else entry.target.classList.remove('fade')
     });
 });
 
-hiddenElements.forEach((el) => observer.observe(el));
+fadeElements.forEach((el) => section_main.observe(el));
+
+// applying left slide transition section_1
+
+const slideElements = document.querySelectorAll('.slide');
+
+const section_1 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+
+        if (entry.isIntersecting) entry.target.classList.add('slide_on');
+        else entry.target.classList.remove('slide_on')
+    });
+});
+
+slideElements.forEach((el) => section_1.observe(el));
